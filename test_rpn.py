477f03eb39e499bd6bb98daf9336c4bb67645fe5
Zerses Cooper <zersesc@umich.edu>
2018-11-07 15:36:48 -0500

    Readded exponent test

--- test_rpn.py
+++ test_rpn.py
@@ -15,6 +15,9 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_exponent(self):
+        result = rpn.calculate("2 3 ^")
+        self.assertEqual(8, result)
 
 if __name__ == '__main__':
     unittest.main()
